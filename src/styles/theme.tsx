3c8bc4ed1819c7791e74ddbf44cc8392d5403dea
Emmanuel <emejia@atx.mx>
2023-02-16 20:05:10 -0600

    Revert "Revert "Add paper framework to desing""
    This reverts commit 7e88678ba02955b8f9c93b67ca33d19b4c895f2f.

---
+++ src/styles/theme.tsx
@@ -0,0 +1,73 @@
+import {
+  configureFonts,
+  MD3LightTheme as DefaultTheme,
+} from 'react-native-paper';
+
+const fontConfig = {
+  ios: {
+    regular: {
+      fontFamily: 'Bahnschrift',
+      fontWeight: 'normal',
+    },
+    medium: {
+      fontFamily: 'Bahnschrift',
+      fontWeight: 'bold',
+    },
+    light: {
+      fontFamily: 'Bahnschrift',
+      fontWeight: 'normal',
+    },
+    thin: {
+      fontFamily: 'Bahnschrift',
+      fontWeight: 'normal',
+    },
+  },
+  android: {
+    regular: {
+      fontFamily: 'Bahnschrift',
+      fontWeight: 'normal',
+    },
+    medium: {
+      fontFamily: 'Bahnschrift',
+      fontWeight: 'bold',
+    },
+    light: {
+      fontFamily: 'Bahnschrift',
+      fontWeight: 'normal',
+    },
+    thin: {
+      fontFamily: 'Bahnschrift',
+      fontWeight: 'normal',
+    },
+  },
+};
+
+const colors = {
+  primary: '#69F6CC',
+  gray: '#38383A',
+  dark_gray: '#1C1C1E',
+  white: '#F2F2F7',
+  error: '#FF003E',
+  gray_variant: '#8E8E93',
+};
+
+const theme = {
+  ...DefaultTheme,
+  // Specify custom property
+  myOwnProperty: true,
+  // Specify custom property in nested object
+  colors: {
+    ...DefaultTheme.colors,
+    primary: colors.primary,
+    surfaceVariant: colors.gray,
+    background: colors.dark_gray,
+    outlineVariant: colors.gray,
+    outline: colors.primary,
+    whiteVariant: colors.white,
+    error: colors.error,
+    variantGray: colors.gray_variant,
+  },
+  fonts: configureFonts({config: fontConfig, isV3: true}),
+};
+
+export default theme;
