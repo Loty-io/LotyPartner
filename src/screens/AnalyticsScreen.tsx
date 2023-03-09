bba51843406b19f8ffffffa18e21f306a5c0580c
ShuraRB <81206040+ShuraRB@users.noreply.github.com>
2023-03-08 18:34:40 -0600

    cleanned screens
    Se hizo limpieza de las screens, quitando componentes innecesarios, lo mismo con los estilos.

--- src/screens/AnalyticsScreen.tsx
+++ src/screens/AnalyticsScreen.tsx
@@ -1,27 +1,23 @@
 import * as React from 'react';
 import {
   View,
-  StyleSheet,
-  Image,
-  TouchableOpacity,
   SafeAreaView,
   FlatList,
   RefreshControl,
 } from 'react-native';
 
-import BoldCustomText from '../components/BoldCustomText';
 import { countCheckIn, truncateAddress } from '../helpers/utils';
 import { getScannedNfts } from '../helpers/api';
 
-import { Button, Text, useTheme } from 'react-native-paper';
-import { AnimatedFAB } from 'react-native-paper';
+import CustomAppBar from '../components/CustomAppBar';
+import CustomFAB from '../components/CustomFAB';
 
-const AnalyticsScreen = ({ navigation, route, animateFrom }: any) => {
+import { Card, Text, useTheme } from 'react-native-paper';
+
+const AnalyticsScreen = ({ navigation, route }: any) => {
   const theme = useTheme();
   const [scannedNfts, setScannedNfts] = React.useState([]);
-
   const [checkinData, setCheckinData] = React.useState('');
-
   const [isLoading, setIsLoading] = React.useState(true);
   const {
     name: collectionName,
@@ -29,8 +25,13 @@ const AnalyticsScreen = ({ navigation, route, animateFrom }: any) => {
     contractAddress,
     description,
   } = route.params;
-  const [floatinButtonExtend, setFloatinButtonExtend] = React.useState(false);
-  const fabStyle = { [animateFrom]: 16 };
+
+  const [isExtended, setIsExtended] = React.useState(true);
+  const onScroll = ({ nativeEvent }) => {
+    const currentScrollPosition =
+      Math.floor(nativeEvent?.contentOffset?.y) ?? 0;
+    setIsExtended(currentScrollPosition <= 0);
+  };
 
   const fetchData = async () => {
     try {
@@ -38,27 +39,22 @@ const AnalyticsScreen = ({ navigation, route, animateFrom }: any) => {
         id,
         contractAddress,
       );
-
       if (hasError) {
         throw new Error(errorMessage);
       }
-      // console.log(nfts);
       setScannedNfts(nfts);
       const chekingData = await countCheckIn(nfts);
-      console.log(JSON.stringify(chekingData, null, 2));
       setCheckinData(chekingData);
-      console.log(checkinData);
     } catch (error) {
       console.log(error);
     }
-
     setIsLoading(false);
   };
 
   React.useEffect(() => {
     fetchData();
-    console.log(checkinData);
   }, []);
+  
   const qrCollection = () => {
     navigation.navigate('QR', {
       id,
@@ -72,79 +68,45 @@ const AnalyticsScreen = ({ navigation, route, animateFrom }: any) => {
   };
 
   const renderItem = ({ item: { name = '', owner = '', checink = '' } }) => (
-    <View
-      style={{
-        flexDirection: 'row',
-      }}>
-      <View
-        style={{
-          borderBottomColor: '#48484A',
-          borderBottomWidth: 1,
-          padding: 16,
-          flex: 2,
-        }}>
+    <Card contentStyle={{flexDirection:'row'}} 
+      style={{backgroundColor: theme.colors.backdrop, borderWidth: 1, margin: 2}}>
+      <View style={{margin: 10}}>
         <Text variant="titleLarge" style={{ color: theme.colors.surface }}>
           {name}
         </Text>
         <Text
           variant="bodyLarge"
-          style={{ color: theme.colors.inverseOnSurface }}>
+          style={{ color: theme.colors.outline }}>
           {truncateAddress(owner)}
         </Text>
       </View>
-
-      <View
-        style={{
-          borderBottomColor: theme.colors.primary,
-          borderBottomWidth: 1,
-          alignItems: 'flex-end',
-          justifyContent: 'center',
-          paddingRight: 70,
-          flex: 1,
-        }}>
-        <Text variant="bodyLarge" style={{ color: theme.colors.primary }}>
-          {checink}
-        </Text>
-      </View>
-    </View>
+      <Card.Content style={{
+        position: 'absolute', 
+        right: 10, 
+        alignSelf:'center', 
+        borderBottomColor: theme.colors.primary, 
+        borderBottomWidth:1}}>
+      <Text variant="bodyLarge" style={{ color: theme.colors.primary,}}>
+        {checink}
+      </Text>
+      </Card.Content>
+    </Card>
   );
 
   return (
-    <SafeAreaView style={styles.container}>
-      <View
-        style={{
-          justifyContent: 'space-between',
-          alignItems: 'center',
-          height: 50,
-          width: '100%',
-          borderBottomColor: 'black',
-          borderBottomWidth: 1,
-          flexDirection: 'row',
-        }}>
-        <Button
-          style={{
-            flex: 1,
-            alignItems: 'flex-start',
-          }}
-          onPress={() => navigation.goBack()}
-          icon={({}) => (
-            <Image
-              source={require('../assets/arrow-back.png')}
-              style={{ justifyContent: 'center', alignSelf: 'center' }}
-            />
-          )}>
-          {}
-        </Button>
-        <Text style={{ color: theme.colors.surface, flex: 2 }}>
-          {' '}
-          {collectionName}
-        </Text>
-        <View style={{ flex: 1 }} />
-      </View>
-
+    <SafeAreaView style={{flex:1, backgroundColor:theme.colors.background}}>
+      <CustomAppBar
+        title={collectionName}
+        isBack={true}
+        leftButtonText={'Back'}
+        textButtonStyle={{ fontSize: 17 }}
+        onPressLeftButton={onPressGoBack}
+        isRightButton={false}/>
+      
       {checkinData.length ? (
         <FlatList
           data={checkinData}
+          onScroll={onScroll}
           renderItem={renderItem}
           keyExtractor={(_, index) => index.toString()}
           showsVerticalScrollIndicator={false}
@@ -154,78 +116,21 @@ const AnalyticsScreen = ({ navigation, route, animateFrom }: any) => {
         />
       ) : (
         <View
-          style={{
-            justifyContent: 'center',
-            alignItems: 'center',
-            flex: 1,
-          }}>
-          <BoldCustomText
-            style={{
-              color: 'white',
-              fontSize: 18,
-              textAlign: 'center',
-            }}>
+          style={{ justifyContent: 'center', alignItems: 'center', flex: 1 }}>
+          <Text variant='titleLarge' style={{color:theme.colors.surface}}>
             {isLoading ? 'Loading...' : 'Nothing scanned yet'}
-          </BoldCustomText>
+          </Text>
         </View>
       )}
-
-      <AnimatedFAB
-        color={theme.colors.background}
-        icon={({}) => (
-          <Image
-            source={require('../assets/qr_code_scanner.png')}
-            style={{
-              justifyContent: 'center',
-              alignSelf: 'center',
-              // backgroundColor: theme.colors.background,
-              borderRadius: 2,
-            }}
-          />
-        )}
-        label={'Generate QR'}
-        extended={floatinButtonExtend}
-        onPress={() => {
-          !floatinButtonExtend
-            ? setFloatinButtonExtend(true)
-            : (setFloatinButtonExtend(false), qrCollection());
-        }}
-        visible={true}
-        animateFrom={'right'}
-        // iconMode={'static'}
-        style={{ ...styles.fabStyle, backgroundColor: theme.colors.primary }}
+      <CustomFAB 
+        text={'Invite new customer'} 
+        textStyle={{color: theme.colors.surfaceVariant, fontSize: 17, }}
+        onPress = {()=>qrCollection()} 
+        isExtended={isExtended}
+        icon={require('../assets/qr_code_scanner.png')}
       />
     </SafeAreaView>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#1C1C1E',
-  },
-  fabStyle: {
-    bottom: 50,
-    right: 16,
-    position: 'absolute',
-  },
-  centerText: {
-    flex: 1,
-    fontSize: 18,
-    padding: 32,
-    color: '#777',
-  },
-  textBold: {
-    fontWeight: '500',
-    color: '#000',
-  },
-  buttonText: {
-    fontSize: 21,
-    color: 'rgb(0,122,255)',
-  },
-  buttonTouchable: {
-    padding: 16,
-  },
-});
 
 export default AnalyticsScreen;
--- src/screens/CollectionQR.tsx
+++ src/screens/CollectionQR.tsx
@@ -1,104 +1,50 @@
 import React from 'react';
 import {
   View,
-  Text,
-  StyleSheet,
   SafeAreaView,
-  TouchableOpacity,
-  Image,
   Dimensions,
   ScrollView,
 } from 'react-native';
-import CustomText from '../components/CustomText';
+
+import CustomAppBar from '../components/CustomAppBar';
+import theme from '../styles/theme';
+
 import QRCode from 'react-native-qrcode-svg';
-import BoldCustomText from '../components/BoldCustomText';
+import { Card, Text } from 'react-native-paper';
 
 const paddingHorizontal = 10;
-const borderRadius = 18;
-const screenWidth = Dimensions.get('window').width;
 
 const CollectionQR = ({ navigation, route }: any) => {
   const { collectionName, id, contractAddress, description } = route.params;
 
-  // para prueba const contractAddress: string = "0xB153f3Abb4a2Aecd8A20dda2cC2BBF3E75bcb56f";
-  console.log(contractAddress);
   const onPressGoBack = () => {
     navigation.goBack();
   };
 
   return (
-    <SafeAreaView style={styles.container}>
-      <View
-        style={{
-          justifyContent: 'space-between',
-          alignItems: 'center',
-          height: 50,
-          width: '100%',
-          paddingHorizontal: 16,
-          borderBottomColor: 'black',
-          borderBottomWidth: 1,
-          flexDirection: 'row',
-        }}>
-        <TouchableOpacity
-          onPress={onPressGoBack}
-          style={{ flexDirection: 'row', alignItems: 'center' }}>
-          <Image source={require('../assets/arrow-back.png')} />
-          {/* <CustomText
-                        style={{
-                            color: '#69F6CC',
-                            fontSize: 17,
-                            marginLeft: 10,
-                            marginTop: 1,
-                        }}>
-                        Back
-                    </CustomText> */}
-        </TouchableOpacity>
-        <CustomText
-          style={{
-            color: 'white',
-            fontSize: 17,
-          }}>
-          QR Code
-        </CustomText>
-        <CustomText
-          style={{
-            color: '#1C1C1E',
-            fontSize: 17,
-          }}>
-          a.
-        </CustomText>
-      </View>
+    <SafeAreaView style={{flex: 1, backgroundColor: theme.colors.background}}>
+      <CustomAppBar
+        title={''}
+        isBack={true}
+        leftButtonText={'Back'}
+        textButtonStyle={{ fontSize: 17 }}
+        onPressLeftButton={onPressGoBack}
+        isRightButton={false}/>
       <ScrollView>
-        <View
-          style={{
-            justifyContent: 'center',
-            alignItems: 'center',
-            paddingHorizontal,
-            marginTop: 30,
-            marginBottom: 40,
-          }}>
-          <BoldCustomText
-            style={{
-              color: 'white',
-              fontSize: 28,
-              borderBottomColor: 'white',
-              borderBottomWidth: 1,
-            }}>
+        <Card contentStyle={{alignItems: 'center', paddingHorizontal: paddingHorizontal, paddingVertical: 15}} 
+          style={{backgroundColor: theme.colors.background}}>
+          <Text variant='headlineMedium'
+            style={{color: theme.colors.surface, marginVertical: 25}}>
             {collectionName}
-          </BoldCustomText>
-
+          </Text>
           <View
             style={{
               width: '100%',
               height: Dimensions.get('window').width - paddingHorizontal * 2,
               backgroundColor: 'white',
-              borderWidth: 1,
-              borderColor: 'transparent',
-              borderRadius,
+              borderRadius: 10,
               justifyContent: 'center',
               alignItems: 'center',
-              marginTop: 40,
-              marginBottom: 40,
             }}>
             <QRCode
               value={contractAddress}
@@ -108,42 +54,14 @@ const CollectionQR = ({ navigation, route }: any) => {
               backgroundColor="white"
             />
           </View>
-
-          <CustomText
-            style={{
-              color: 'white',
-              fontSize: 18,
-              textAlign: 'justify',
-            }}>
+          <Text variant='titleMedium'
+            style={{color: theme.colors.surface, marginVertical: 25}}>
             {description}
-          </CustomText>
-        </View>
+          </Text>
+        </Card>
       </ScrollView>
     </SafeAreaView>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#1C1C1E',
-  },
-  centerText: {
-    flex: 1,
-    fontSize: 18,
-    padding: 32,
-    color: '#777',
-  },
-  textBold: {
-    fontWeight: '500',
-    color: '#000',
-  },
-  buttonText: {
-    fontSize: 21,
-    color: 'rgb(0,122,255)',
-  },
-  buttonTouchable: {
-    padding: 16,
-  },
-});
 export default CollectionQR;
